e5bac07ce856ce8cc4c6ba4a982489ee8984b972
Rafael Langoni <langoninaves@gmail.com>
2024-12-03 05:45:35 -0330

    first commit; code template

---
+++ src/test/java/com/langoni/picpay_challenge/PicpayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.langoni.picpay_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
